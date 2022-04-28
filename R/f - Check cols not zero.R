620ae7bf26272a6c78e6ba01e3c255d8f701caaa
pieterov <pieter.overdevest@mac.com>
2022-04-28 17:32:27 +0200

    v85 - New - Check cols not zero - Update - Get latest file

---
+++ R/f - Check cols not zero.R
@@ -0,0 +1,102 @@
+##############################################################################################
+# NAME:         FUNCTION - CHECK COLS NOT ZERO
+# AUTHOR:       Pieter Overdevest
+##############################################################################################
+
+        f_check_cols_not_zero <- function(
+
+                df.input,
+                v.col,
+                c.id
+        ) {
+
+
+        ######################################################################################
+        # TEST
+        ######################################################################################
+
+        # df.input = data.frame(ID = c(11,12,13,14,15), pieter = c(1,2,0,0,0), bart = c(NA,NA,0,4,0))
+        # v.col    = c("pieter", "bart")
+        # c.id     = "ID"
+        # f_check_cols_not_zero(df.input, v.col, c.id)
+
+        # df.input = df.datachamp.baseline.source %>% filter(Status == "ACTIVE")
+        # v.col    = v.feature.cannot.be.empty
+
+
+        ######################################################################################
+        # ERROR CHECKS
+        ######################################################################################
+
+        # Check presence of columns in df.input.
+        f_check_cols_present(df.input, c.id)
+        f_check_cols_present(df.input, v.col)
+
+        # Check that c.id does not contain NA and is unique
+        f_check_col_not_empty(df.input, c.id)
+        f_check_col_unique(df.input, c.id)
+
+
+        ######################################################################################
+        # PROCESS
+        ######################################################################################
+
+        # Determine features with 0.
+        df.temp <- df.input %>%
+
+                # Select concerned columns
+                select(all_of(v.col)) %>%
+
+                # Determine number of NA per column
+                f_info_per_column(b.view = FALSE, b.return = TRUE) %>%
+
+                # Select columns with NA
+                filter(n.zero > 0) %>%
+
+                # Create label.
+                mutate(n.label = paste0("'", feature, "' (", n.zero, ")"))
+
+
+        ######################################################################################
+        # ERROR CHECK
+        ######################################################################################
+
+        if(nrow(df.temp) > 0) {
+
+                # Comms.
+                cat(paste0(
+                        "We observe features with ZERO values (out of ",
+                        nrow(df.input), "): ", f_paste(df.temp$n.label), ".\n\n"
+                ))
+
+
+                # Create label.
+                v.temp <- lapply(df.temp$feature, function(c.temp) { # c.temp = df.temp$feature[1]
+
+                        paste0(
+                                "'", c.temp, "' (",
+
+                                f_paste(
+
+                                        df.input %>%
+
+                                                select(all_of(c(c.id, c.temp))) %>%
+
+                                                filter(get(c.temp) == 0) %>%
+
+                                                pull(all_of(c.id))
+                                ), ")"
+                        )
+
+                }) %>% unlist()
+
+
+                stop(paste0(
+
+                        "Note, the following features contain one ore more ZERO values, see '", c.id, "': ",
+                        f_paste(v.temp)
+                ))
+        }
+
+        }
+
--- R/f - Get latest file.R
+++ R/f - Get latest file.R
@@ -32,165 +32,177 @@
 # Initialize data.
 ##############################################################################
 
-                # Determine c.pattern based on whether the exact string/filename needs to be found.
-                c.pattern <- ifelse(b.exact.match,
+        # Add escape characters before '(' and ')', if present.
+        if(grepl("\\(", c.file.string)) {
 
-                                    paste0("^", c.file.string, "\\.", c.file.type, "[xm]?$"),
+                c.file.string <- gsub("\\(", "\\\\(", c.file.string)
+        }
 
-                                    paste0(c.file.string, ".*\\.", c.file.type, "[xm]?$"))
+        if(grepl("\\)", c.file.string)) {
+
+                c.file.string <- gsub("\\)", "\\\\)", c.file.string)
+        }
+
+
+        # Determine c.pattern based on whether the exact string/filename needs to be found.
+        c.pattern <- ifelse(b.exact.match,
+
+                            paste0("^", c.file.string, "\\.", c.file.type, "[xm]?$"),
+
+                            paste0(c.file.string, ".*\\.", c.file.type, "[xm]?$"))
 
 
 ##############################################################################
 # Get latest file.
 ##############################################################################
 
-                # Get all files in the path folder that match c.pattern.
-                df.file <- tibble(
+        # Get all files in the path folder that match c.pattern.
+        df.file <- tibble(
 
-                        files = list.files(
+                files = list.files(
 
-                                path        = c.path,
-                                recursive   = FALSE,
-                                full.names  = FALSE,
-                                ignore.case = TRUE,
-                                pattern     = c.pattern
-                                )
+                        path        = c.path,
+                        recursive   = FALSE,
+                        full.names  = FALSE,
+                        ignore.case = TRUE,
+                        pattern     = c.pattern
                         )
+                )
 
 
-                # Remove files that contain string in c.file.string.exclude.
-                if(!is.null(c.file.string.exclude)) {
+        # Remove files that contain string in c.file.string.exclude.
+        if(!is.null(c.file.string.exclude)) {
 
-                        df.file <- df.file %>%
+                df.file <- df.file %>%
 
-                                filter(!grepl(c.file.string.exclude, files))
-                }
-
-
-                # Check whether a file was found.
-                if (nrow(df.file) == 0) {
-
-                        stop(paste0("There was no file found with string '", c.file.string,
-                                    "' and of type '", c.file.type, "' in path '", c.path, ".'"))
-                }
-
-                # Clean up the file names and get the concerned file name.
-                df.type <- df.file %>%
-
-                        # Check version number in the filename.
-                        mutate(
-
-                                # Define full path.
-                                path.file = paste0(c.path, files),
-
-                                # Get the version number from the filename (v.. followed by number). In case,
-                                # there is no version number present in the filename, assign 'v1.'
-                                version.number = str_extract(paste(files, "v1"), "[vV][0-9]+"),
-                                version.number = gsub("[vV]", "", version.number),
-                                version.number = as.numeric(version.number),
-
-                                # Get boolean to express if file is highest version.
-                                highest.version = ifelse(version.number == max(version.number), TRUE, FALSE)) %>%
-
-                        # Filter the file(s) with the highest version number.
-                        filter(highest.version) %>%
-
-                        # Check date of modification of the file.
-                        mutate(
-
-                                # Get the last modified date for file.
-                                date.last.mod = file.info(path.file)$mtime,
-
-                                # Get boolean to express if file is the most recently modifed version.
-                                most.recent.mod.version = ifelse(date.last.mod == max(date.last.mod), TRUE, FALSE)) %>%
-
-                        # Filter the file with the most recent date of modification.
-                        filter(most.recent.mod.version == TRUE)
+                        filter(!grepl(c.file.string.exclude, files))
+        }
 
 
-                # Communicate stats to the user.
-                c.path.file <- as.character(df.type$path.file)
+        # Check whether a file was found.
+        if (nrow(df.file) == 0) {
 
-                # If filetype is 'xls''
-                if(c.file.type == "xls") {
+                stop(paste0("There was no file found with string '", c.file.string,
+                            "' and of type '", c.file.type, "' in path '", c.path, ".'"))
+        }
 
-                        v.sheet.name <- readxl::excel_sheets(c.path.file)
+        # Clean up the file names and get the concerned file name.
+        df.type <- df.file %>%
 
-                        if(is.null(c.sheet.name)) {
+                # Check version number in the filename.
+                mutate(
 
-                                c.sheet.name.read <- v.sheet.name[1]
+                        # Define full path.
+                        path.file = paste0(c.path, files),
 
-                                if(length(v.sheet.name) > 1) {
+                        # Get the version number from the filename (v.. followed by number). In case,
+                        # there is no version number present in the filename, assign 'v1.'
+                        version.number = str_extract(paste(files, "v1"), "[vV][0-9]+"),
+                        version.number = gsub("[vV]", "", version.number),
+                        version.number = as.numeric(version.number),
 
-                                        warning(paste0(
+                        # Get boolean to express if file is highest version.
+                        highest.version = ifelse(version.number == max(version.number), TRUE, FALSE)) %>%
 
-                                                "Note, you did not supply a value for 'c.sheet.name' and we found more than ",
-                                                "one sheet in the workbook: ", f_paste(v.sheet.name, b.quotation = TRUE),
-                                                ". We read the data from the first sheet: '", c.sheet.name.read, "'."
-                                        ))
-                                }
+                # Filter the file(s) with the highest version number.
+                filter(highest.version) %>%
 
-                        } else {
+                # Check date of modification of the file.
+                mutate(
 
-                                b.sheet.name <- grepl(c.sheet.name, v.sheet.name)
+                        # Get the last modified date for file.
+                        date.last.mod = file.info(path.file)$mtime,
 
-                                if(sum(b.sheet.name) == 0) {
+                        # Get boolean to express if file is the most recently modifed version.
+                        most.recent.mod.version = ifelse(date.last.mod == max(date.last.mod), TRUE, FALSE)) %>%
 
-                                        stop(paste0(
+                # Filter the file with the most recent date of modification.
+                filter(most.recent.mod.version == TRUE)
 
-                                                "Note, we found no sheet that contains the string: '",
-                                                c.sheet.name, "'. We did find the following sheets in the workbook: ",
-                                                f_paste(v.sheet.name, b.quotation = TRUE), "!"
-                                        ))
-                                }
 
-                                if(sum(b.sheet.name) > 1) {
+        # Communicate stats to the user.
+        c.path.file <- as.character(df.type$path.file)
 
-                                        stop(paste0(
+        # If filetype is 'xls''
+        if(c.file.type == "xls") {
 
-                                                "Note, we found more than one sheet that contains the string: '",
-                                                c.sheet.name, "', namely: ",
-                                                f_paste(v.sheet.name[b.sheet.name], b.quotation = TRUE),
-                                                ". Please better specify c.sheet.name and/or use regex ('^', '$')!"
-                                        ))
-                                }
+                v.sheet.name <- readxl::excel_sheets(c.path.file)
 
-                                c.sheet.name.read <- v.sheet.name[b.sheet.name]
+                if(is.null(c.sheet.name)) {
+
+                        c.sheet.name.read <- v.sheet.name[1]
+
+                        if(length(v.sheet.name) > 1) {
+
+                                warning(paste0(
+
+                                        "Note, you did not supply a value for 'c.sheet.name' and we found more than ",
+                                        "one sheet in the workbook: ", f_paste(v.sheet.name, b.quotation = TRUE),
+                                        ". We read the data from the first sheet: '", c.sheet.name.read, "'."
+                                ))
                         }
+
+                } else {
+
+                        b.sheet.name <- grepl(c.sheet.name, v.sheet.name)
+
+                        if(sum(b.sheet.name) == 0) {
+
+                                stop(paste0(
+
+                                        "Note, we found no sheet that contains the string: '",
+                                        c.sheet.name, "'. We did find the following sheets in the workbook: ",
+                                        f_paste(v.sheet.name, b.quotation = TRUE), "!"
+                                ))
+                        }
+
+                        if(sum(b.sheet.name) > 1) {
+
+                                stop(paste0(
+
+                                        "Note, we found more than one sheet that contains the string: '",
+                                        c.sheet.name, "', namely: ",
+                                        f_paste(v.sheet.name[b.sheet.name], b.quotation = TRUE),
+                                        ". Please better specify c.sheet.name and/or use regex ('^', '$')!"
+                                ))
+                        }
+
+                        c.sheet.name.read <- v.sheet.name[b.sheet.name]
                 }
+        }
 
 
-                if (c.show.report != "none") {
+        if (c.show.report != "none") {
 
-                        if(c.show.report == "all") {
+                if(c.show.report == "all") {
 
-                                cat(paste0("\nRead as  : ", c.file.type))
+                        cat(paste0("\nRead as  : ", c.file.type))
 
-                                cat(paste0("\nString   : ", c.file.string))
+                        cat(paste0("\nString   : ", c.file.string))
 
-                                cat(paste0("\nFile     : ", df.type$files))
+                        cat(paste0("\nFile     : ", df.type$files))
 
-                                if (c.file.type == "xls") {
+                        if (c.file.type == "xls") {
 
-                                        cat(paste0("\nSheet    : '", c.sheet.name, "' (requested), '", c.sheet.name.read, "' (read)"))
-                                }
+                                cat(paste0("\nSheet    : '", c.sheet.name, "' (requested), '", c.sheet.name.read, "' (read)"))
+                        }
 
-                                cat(paste0("\nPath     : ", c.path))
+                        cat(paste0("\nPath     : ", c.path))
 
-                                cat(paste0("\nLast mod : ", df.type$date.last.mod))
+                        cat(paste0("\nLast mod : ", df.type$date.last.mod))
 
-                                cat("\n\n")
+                        cat("\n\n")
 
-                                print(round(difftime(Sys.time(), df.type$date.last.mod, units = "auto"), 1))
+                        print(round(difftime(Sys.time(), df.type$date.last.mod, units = "auto"), 1))
 
-                                cat("\n==========================\n")
+                        cat("\n==========================\n")
 
-                        } else {
+                } else {
 
-                                cat(paste0(df.type$files, " - ", df.type$date.last.mod, " - "))
-                        } }
+                        cat(paste0(df.type$files, " - ", df.type$date.last.mod, " - "))
+                } }
 
-                # Return file, incl path.
-                return(c.path.file)
+        # Return file, incl path.
+        return(c.path.file)
 
-                }
+        }

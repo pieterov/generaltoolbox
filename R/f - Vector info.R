bed9a3308f860499ddc05500868cf459c2fe90da
pieterov <pieter.overdevest@mac.com>
2022-05-08 07:47:13 +0200

    v90 - f_info shows -Inf

--- R/f - Vector info.R
+++ R/f - Vector info.R
@@ -9,160 +9,216 @@
                               n.top,
                               show.freq) {
 
-    ##############################################################################
-    # Error check.
-    ##############################################################################
+##############################################################################
+# Error check.
+##############################################################################
 
-    # v         = df.discount.too.low$discount
-    # name      = "df.discount.too.low$discount"
-    # n.top     = 10
-    # show.freq = TRUE
+        # v         = df.discount.too.low$discount
+        # name      = "df.discount.too.low$discount"
+        # n.top     = 10
+        # show.freq = TRUE
 
-    # v <- c(0, 2, 2, NA, NA, NA, 0/0, 0/0, 0/0, 0/0, 6/0, 7/0, 8/0, 9/0, 10/0)
-    # v <- c(NA, NA, NA)
-    # v <- c(0/0, 0/0, 0/0, 0/0)
-    # v <- c(6/0, 7/0, 8/0, 9/0, 10/0)
-
-
-    ##############################################################################
-    # Error check.
-    ##############################################################################
-
-    if(!is.numeric(n.top) & n.top != "all") {
-
-        stop("n.top moet valide waarde bevatten: integer of 'all'")
-    }
+        # v <- c(0, 2, 2, NA, NA, NA, 0/0, -0/0, -0/0, 0/0, 6/0, -7/0, -8/0, 9/0, 10/0)
+        # v <- c(NA, NA, NA)
+        # v <- c(0/0, 0/0, 0/0, 0/0)
+        # v <- c(6/0, 7/0, 8/0, 9/0, 10/0)
 
 
-    ##############################################################################
-    # Analyse data.
-    ##############################################################################
+##############################################################################
+# Error check.
+##############################################################################
 
-    # Calculate basic info.
-    df.basic.info <- data.frame(x  = c("Total elements:",
-                                       "Unique elements:",
-                                       "NA:",
-                                       "NaN:",
-                                       "Inf:",
-                                       "0:",
-                                       "Empty:"),
+        if(!is.numeric(n.top) & n.top != "all") {
 
-                                y = c(length(v),
-                                      length(unique(v)),
-                                      sum(v %in% NA),
-                                      sum(v %in% NaN),
-                                      sum(v %in% Inf),
-                                      sum(v == 0, na.rm = TRUE),
-                                      sum(v == "", na.rm = TRUE)),
-
-                                z = c("",
-                                      "",
-                                      paste(round(
-                                              sum(v %in% NA) / length(v) * 100,
-                                              digits = 1),
-                                            "%"),
-
-                                      paste(round(
-                                              sum(v %in% NaN) / length(v) * 100,
-                                              digits = 1),
-                                            "%"),
-
-                                      paste(round(
-                                              sum(v %in% Inf) / length(v) * 100,
-                                              digits = 1),
-                                            "%"),
-
-                                      paste(round(
-                                              sum(v == 0, na.rm = TRUE) / length(v) * 100,
-                                              digits = 1),
-                                            "%"),
-
-                                      paste(round(
-                                              sum(v == "", na.rm = TRUE) / length(v) * 100,
-                                              digits = 1),
-                                            "%")
-                                      ))
+                stop("n.top moet valide waarde bevatten: integer of 'all'")
+        }
 
 
-    names(df.basic.info) <- c("============================", "======", "======")
+##############################################################################
+# Analyse data.
+##############################################################################
 
-    # Print header.
-    cat(paste0("\n ", name, "\n"))
+        # Calculate basic info.
+        df.basic.info <- data.frame(
 
-    cat(paste0(rep(" ", 29), collapse = ""), "n      perc\n")
+                x  = c(
+                        "Total elements:",
+                        "Unique elements:",
+                        "0:",
+                        "Empty:",
+                        "NA:",
+                        "NaN:",
+                        "Inf(-):",
+                        "Inf(+):"
+                ),
 
-    # Show in console, left align.
-    print(x         = df.basic.info,
-          row.names = FALSE,
-          right     = FALSE)
+                y = c(
+                        length(v),
+                        length(unique(v)),
+                        sum(v == 0, na.rm = TRUE),
+                        sum(v == "", na.rm = TRUE),
+                        sum(v %in% NA),
+                        sum(v %in% NaN),
+                        sum(v %in% -Inf),
+                        sum(v %in% Inf)
+                ),
+
+                z = c(
+                        "",
+                        "",
+
+                        paste(
+                                round(
+                                        sum(v == 0, na.rm = TRUE) / length(v) * 100,
+                                        digits = 1
+                                ),
+                                "%"
+                        ),
+
+                        paste(
+                                round(
+                                        sum(v == "", na.rm = TRUE) / length(v) * 100,
+                                        digits = 1
+                                ),
+                                "%"
+                        ),
+
+                        paste(
+                                round(
+                                        sum(v %in% NA) / length(v) * 100,
+                                        digits = 1
+                                ),
+                                "%"
+                        ),
+
+                        paste(
+                                round(
+                                        sum(v %in% NaN) / length(v) * 100,
+                                        digits = 1
+                                ),
+                                "%"
+                        ),
+
+                        paste(
+                                round(
+                                        sum(v %in% -Inf) / length(v) * 100,
+                                        digits = 1
+                                ),
+                                "%"
+                        ),
+
+                        paste(
+                                round(
+                                        sum(v %in% Inf) / length(v) * 100,
+                                        digits = 1
+                                ),
+                                "%"
+                        )
+                )
+        )
 
 
-    # Show frequency table.
-    if (show.freq) {
+        names(df.basic.info) <- c("============================", "======", "======")
 
-            # Replace any NA by "NA", and NaN by "NaN"
-            v[v %in% NA]  <- "NA "
-            v[v %in% NaN] <- "NaN "
-            v[v %in% Inf] <- "Inf "
+        # Print header.
+        cat(paste0("\n ", name, "\n"))
 
-            # Calculate frequency of levels in vector.
-            df.freq.source <- as.data.frame(table(v)) %>%
+        cat(paste0(rep(" ", 29), collapse = ""), "n      perc\n")
 
-                    arrange(desc(Freq), v) %>%
-
-                    mutate(perc = Freq / sum(Freq) * 100)
-
-
-            df.dots <- data.frame(v    = "...",
-                                  Freq = "...",
-                                  perc = "..."
-                                  )
+        # Show in console, left align.
+        print(
+                x         = df.basic.info,
+                row.names = FALSE,
+                right     = FALSE
+        )
 
 
-            df.total <- data.frame(v    = c("----------------", "TOTAL"),
-                                   Freq = c("------", sum(df.freq.source$Freq)),
-                                   perc = c("------", paste(round(sum(df.freq.source$perc), digits = 1), "%"))
-                                   )
+        # Show frequency table.
+        if (show.freq) {
+
+                # Replace any NA by "NA", and NaN by "NaN"
+                v[v %in% NA]  <- "NA "
+                v[v %in% NaN] <- "NaN "
+                v[v %in% Inf] <- "Inf "
+
+                # Calculate frequency of levels in vector.
+                df.freq.source <- as.data.frame(table(v)) %>%
+
+                        arrange(desc(Freq), v) %>%
+
+                        mutate(perc = Freq / sum(Freq) * 100)
 
 
-            df.freq <- df.freq.source %>%
+                df.dots <- data.frame(
 
-                    mutate(perc = paste(round(df.freq.source$perc, digits = 1), "%")) %>%
-
-                    head(n.top)
-
-
-            # Puntjes toevoegen als n.top een getal is.
-            if(is.numeric(n.top)) {
-
-                df.freq <- rbind(df.freq, df.dots)
-            }
-
-            # Total toevoegen.
-            df.freq <- rbind(df.freq, df.total)
-
-            names(df.freq) <- c("============================", "======", "======")
+                        v    = "...",
+                        Freq = "...",
+                        perc = "..."
+                )
 
 
+                df.total <- data.frame(
 
-            # Header frequency table.
-            cat(paste0("\n Frequency table ",
-
-                       if(is.numeric(n.top)) {
-
-                            if(nrow(df.freq.source) < n.top)
-
-                                {"(all items): "} else {paste0("(Top-" , n.top, "):    ")}
-
-                            } else {"(all items): "},
-
-                       "n      perc\n"))
+                        v    = c("----------------", "TOTAL"),
+                        Freq = c("------", sum(df.freq.source$Freq)),
+                        perc = c("------", paste(round(sum(df.freq.source$perc), digits = 1), "%"))
+                )
 
 
+                df.freq <- df.freq.source %>%
 
-            print(x         = df.freq,
-                  row.names = FALSE,
-                  right     = FALSE)
+                        mutate(
+                                perc = paste(round(df.freq.source$perc, digits = 1), "%")
+                        ) %>%
 
-            }
-    }
+                        head(n.top)
+
+
+                # Puntjes toevoegen als n.top een getal is.
+                if(is.numeric(n.top)) {
+
+                        df.freq <- rbind(df.freq, df.dots)
+                }
+
+                # Total toevoegen.
+                df.freq <- rbind(df.freq, df.total)
+
+                names(df.freq) <- c("============================", "======", "======")
+
+
+                # Header frequency table.
+                cat(
+                        paste0(
+
+                                "\n Frequency table ",
+
+                                if(is.numeric(n.top)) {
+
+                                        if(nrow(df.freq.source) < n.top) {
+
+                                                "(all items): "
+
+                                        } else {
+
+                                                paste0("(Top-" , n.top, "):    ")
+                                        }
+
+                                } else {
+
+                                        "(all items): "
+                                },
+
+                                "n      perc\n"
+                        )
+                )
+
+
+
+                print(
+                        x         = df.freq,
+                        row.names = FALSE,
+                        right     = FALSE
+                )
+
+        }
+}

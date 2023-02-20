e95fb8e56ae481ea94b8337d77d4ee20075699f3
pieterov <pieter.overdevest@mac.com>
2023-02-20 14:45:03 +0100

    update in f_distance

--- R/f - Distance.R
+++ R/f - Distance.R
@@ -4,12 +4,13 @@
 #'
 #' @author Pieter Overdevest
 #'
-#' @param x1,y1 Coordinates of first point.
-#' @param x2,y2 Coordinates of second point.
+#' @param x1,y1 Coordinates of first point (default: y1 = NULL).
+#' @param x2,y2 Coordinates of second point (default: x2, y2 = NULL).
 #'
 #' @returns Vector with distances.
 #'
-#' @details -
+#' @details In case a single vector of 4 elements is provided, then x1, x2, x2, and y2 are
+#' reassigned using x1.
 #'
 #' @export
 #'
@@ -28,8 +29,20 @@
 
         f_distance <- function(
 
-                x1, y1, x2, y2
+                x1, y1 = NULL, x2 = NULL, y2 = NULL
         ) {
+                # In case x1, y1, x2, y2 are supplied in single vector.
+                if(is.null(y1) & is.null(x2) & is.null(y2)) {
+
+                        if(length(x1) != 4) {
+                                stop('If x1, y1, x2, and y2 are provided in single vector, its length must be 4.')
+                        }
+
+                        y1 = x1[2]
+                        x2 = x1[3]
+                        y2 = x1[4]
+                        x1 = x1[1]
+                }
 
                 return(
                         sqrt((x1 - x2)^2 + (y1 - y2)^2)

5df9f433b488d6f3d27cc4b0d949edbaa7f6f26e
pieterov <pieter.overdevest@mac.com>
2024-04-03 09:39:17 +0200

    update in cleaning part in f_paste

--- R/f - Paste.R
+++ R/f - Paste.R
@@ -67,7 +67,7 @@
         # v.string    = df.temp$ID
         # b.quotation = TRUE
 
-        # v.string = "Unexpected zero value in Variant Price (12)"
+        # v.string = NA
         # n.top    = 10
 
 
@@ -75,18 +75,21 @@
 # INITIALIZATION
 #########################################################################
 
-        # Return NULL als input NULL is.
-        if(is.null(v.string) | length(v.string) == 0) return(NULL)
-
-
-        # Return "" als input "" is.
-        if(all(v.string == "")) return("")
-
-
         # Verwijder NA en "" uit lijst.
         v.string <- v.string[!is.na(v.string)]
+        v.string <- v.string[!is.null(v.string)]
         v.string <- v.string[v.string != ""]
 
+        # Return "" als input NULL is.
+        if(
+                is.null(v.string) |
+                is.na(v.string) |
+                length(v.string) == 0
+        )
+                return("")
+
+        # Return "" als input "" is.
+        #if(all(v.string == "")) return("")
 
         # Remove duplicates, if required.
         if(b.unique) {

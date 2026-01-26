8cc3262d72b421af80b16002f9d2aee85beb0ab8
pieterov <pieter.overdevest@mac.com>
2026-01-26 16:19:56 +0100

    v251 - added generaltoolbox.R

---
+++ R/generaltoolbox.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+# This line automatically exports every function that starts with a letter.
+# This saves you from adding "@export" to 50+ files manually.
+#' @exportPattern "^[a-zA-Z]"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
